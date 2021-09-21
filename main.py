748f96fdcec4a312e071d007cb5d54cf63b5ba21
Mihail-Tyurin <91095313+Mihail-Tyurin@users.noreply.github.com>
2021-09-21 13:06:21 +0300

    upd

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm TMN')
+    print_hi('PyCharm TMN 21092021')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
